347f1559264bbf2f45c81d3fa9d076b9d24eabeb
맹옹\twng9 <mengo0ong@gmail.com>
2024-07-04 23:12:15 +0900

    vue update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <Project></Project>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Project from './components/my-first-vue-project.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    Project
   }
 }
 </script>
---
+++ src/components/my-first-vue-project.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>{{ msgA }}</h1>
+    <h1>{{ msgB }}</h1>
+    <button @click="changeMessage()">Change Message</button>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        msgA: "Hello Vue 3!"
+      };
+    },
+    methods: {
+      changeMessage() {
+        this.msgA = "Message changed!";
+        this.msgB = 'Changed message B!';
+      }
+    }
+  };
+</script>
+
+
+
+<style scoped>
+
+</style>
