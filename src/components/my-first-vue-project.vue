b24277141e9b04e39d690a24ff8d2a7103197454
맹옹\twng9 <mengo0ong@gmail.com>
2024-07-04 23:17:58 +0900

    vue 인스턴스 강의 완료

--- src/components/my-first-vue-project.vue
+++ src/components/my-first-vue-project.vue
@@ -15,10 +15,26 @@
       };
     },
     methods: {
+      /* 반응형 확인 */
       changeMessage() {
-        this.msgA = "Message changed!";
+        this.msgA = "Changed message A!";
         this.msgB = 'Changed message B!';
       }
+    },
+    /* computed: 기존에 정의했던 데이터를 추가적으로 계산해서 렌더링 됨 */
+    computed: {},
+    /* 데이터 반응을 관찰 -> 설정한 데이터가 변경될 때 마다 실행함 */
+    watch: {
+      msgA(value){ // msgA가 변경될 때마다 그 값을 콘솔에 표기한다.
+        console.log(value)
+      }
+    },
+    /* 라이프 사이클 */
+    created() {
+
+    },
+    mounted(){
+
     }
   };
 </script>
