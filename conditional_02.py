44576d52b52b56af6d95d44512311d3890881b15
맹옹\twng9 <mengo0ong@gmail.com>
2024-07-05 13:59:50 +0900

    베어유 5강 내용

---
+++ conditional_02.py
@@ -0,0 +1,27 @@
+a = 0
+b = 2
+
+if a<b:
+    print("if조건문 true")
+elif a==b:
+    pass
+elif a>b:
+    pass
+else:
+    pass
+
+
+"""
+사과는 냉장실에 
+아이스크림은 냉동실에 넣어 주시고,
+나머지는 폐기처분 해주세요.
+"""
+
+변수 = "사과"
+
+if 변수 == "사과":
+    print("냉장실")
+elif 변수 == "아이스크림":
+    print("냉동실")
+else:
+    print("폐기처분")
