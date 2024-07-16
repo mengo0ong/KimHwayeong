41c50bab143c4b05e1e6794bf8896ed6982b72d8
맹옹\twng9 <mengo0ong@gmail.com>
2024-07-17 00:12:38 +0900

    "16일차 중간 commit"

--- Anonymous/user/views.py
+++ Anonymous/user/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import make_password
+from user.models import User
 
 # Create your views here.
 def signin(request):
     if request.method == "GET":
         return render(request, 'page/signin.html')
-    
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+
+
 def signup(request):
     if request.method == "GET":
         return render(request, 'page/signup.html')
