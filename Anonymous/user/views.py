4fc4ae6b4d66a4275dc91f6a6d855e56a9036922
맹옹\twng9 <mengo0ong@gmail.com>
2024-07-17 00:46:34 +0900

    "베어유 16일차 로그인 로직 (1)"

--- Anonymous/user/views.py
+++ Anonymous/user/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect
+# Django rib auth (로그인, 로그아웃)
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import make_password
+
+# Django message rib
+from django.contrib import messages
+
+# made User object
 from user.models import User
 
 # Create your views here.
@@ -10,6 +16,15 @@ def signin(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
+
+        # Django 내부적으로 User객체를 반환
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('board')
+        else:
+            messages.error(request, '입력값을 확인해 주세요.')
+            return redirect('signin')
 
 
 
