0db38d3ccdb441a0a2580aa4610179bd51b41886
맹옹\twng9 <mengo0ong@gmail.com>
2024-07-21 21:58:34 +0900

    베어유 20일차 [백엔드/프론트엔드] 검색 기능

--- Anonymous/board/views.py
+++ Anonymous/board/views.py
@@ -8,15 +8,24 @@ def board(request):
         # 해당 페이지를 받을 때 페이지 key값이 없으면 default로 1을 받음
         # 같은 원리로 default값을 받아야할 때에는 .get('name', 'value')
         page = request.GET.get('page', 1)
-        post_set = Post.objects.all().order_by('-id')
+        # 검색
+        search_text = request.GET.get('search_text', "")
+
+        post_set = Post.objects.filter(
+                # i는 대소문자를 가리지 않겠다는 의미, 없어도 됨
+                title__icontains = search_text
+            ).order_by('-id')
         paginator = Paginator(post_set, 4)
 
         # post_set 초기화
         post_set = paginator.get_page(page)
 
+
+
         # 아래의 내용이 html에서 django 태그로 작동한다.
         context = {
-            "post_set": post_set
+            "post_set": post_set,
+            "search_text": search_text
 
         }
 
