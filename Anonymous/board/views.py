14dd833906094148413c83b81f7f97a4e9c62447
맹옹\twng9 <mengo0ong@gmail.com>
2024-07-23 00:04:18 +0900

    "베어유 22일차 게시판 글쓰기 폼 로직 만들기"

--- Anonymous/board/views.py
+++ Anonymous/board/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from board.models import Post
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.core.files.storage import default_storage
+import uuid
 
 def board(request):
     # 게시글 리스트
@@ -31,7 +34,40 @@ def board(request):
 
         # Java의 Controller 같은 느낌
         return render(request, 'page/index.html', context=context)
-    
+
+# 로그인 한 유저만 접근이 가능하도록 함
+@login_required(login_url="signin")  
 def post_write(request):
     if request.method=="GET":
         return render(request, "page/post_write.html")
+    if request.method=="POST":
+        title = request.POST["title"]
+        content = request.POST["content"]
+        img = request.FILES.get("img", None)
+        # 이미지가 들어오지 않았을 때의 초기화
+        img_url = ""
+
+        if img:
+            """
+            이미지 저장 
+            - 이미지를 저장할 수 있는 Default folder 필요
+            - Django 설정 -> settings.py -> static_url 아래에 MEDIA_ROOT='경로' 
+            -> views.py 상단에 from django.core.files.storage import default_storage 
+            """
+
+            # 동일한 파일 이름이 들어왔을 때, 오류가 생기는 것을 방지하기 위해
+            # 파일 이름을 고유하게 지정하기 위해 uuid 사용
+            img_name = uuid.uuid4()
+            # split으로 나눈 배열의 가장 마지막 값
+            img_type = img.name.split('.')[-1]
+            default_storage.save(f"{img_name}.{img_type}", img)
+            img_url = f"{img_name}.{img_type}"
+
+        # Post 객체 저장
+        Post(
+            user = request.user,
+            title = title,
+            content = content,
+            img_url = img_url
+        ).save()
+        return redirect('board')
