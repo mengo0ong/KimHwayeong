34b1f2aad61620f532145de8b16c16aadacd61e0
맹옹\twng9 <mengo0ong@gmail.com>
2024-07-30 13:34:13 +0900

    30일차 AWS 애플리케이션 배포 완료

--- Anonymous/anonymous/settings.py
+++ Anonymous/anonymous/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # 사이트에 들어올 때 허용하는 HOST
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['anonymous-dev.ap-northeast-2.elasticbeanstalk.com', '3.36.58.1']
 
 
 # Application definition
